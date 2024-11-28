55610c8caee731f67e50aa331c0b9ff1712b5b13
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2024-11-28 09:45:35 -0300

    tentativa de css na usuario.php

--- templates/header.php
+++ templates/header.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Sistema de Doação e Recebimento de Tintas - Transforme vidas com cores!">
     <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="stylesheet" href="./CSS/index.css">
+    <link rel="stylesheet" href="./CSS/styles_menu.css">
     <title>Banco de Tintas</title>
 </head>
 <body>
